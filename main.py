aabbd3152265503245d22a1522044b10c50dad2b
TheLastGimbus <mateusz.soszynski@tuta.io>
2021-02-28 15:52:00 +0100

    Change the algo

--- main.py
+++ main.py
@@ -31,20 +31,21 @@ def get_temp():
 
 def main():
     with serial.Serial(args.port, args.baud, timeout=args.timeout) as fan:
+        fan_speed = args.min_speed
         while True:
-            fan_speed = args.min_speed
-            while get_temp() > args.max_temp:
-                print(f'Temperature: {get_temp()}')
+            print(f'Temperature: {get_temp()}')
+            if get_temp() > args.max_temp:
                 if fan_speed < MAX_VALUE:
                     fan_speed += 5
                     print(f'New speed: {fan_speed}')
-                packet = bytearray()
-                packet.append(fan_speed)  # This is where fan value goes
-                fan.write(packet)
-                res = fan.read()
-                if res != b'1':
-                    print(f'Some error: {res}')
-                time.sleep(5)
+            else:
+                fan_speed = args.min_speed
+            packet = bytearray()
+            packet.append(fan_speed)  # This is where fan value goes
+            fan.write(packet)
+            res = fan.read()
+            if res != b'1':
+                print(f'Some error: {res}')
 
             time.sleep(5)
 
